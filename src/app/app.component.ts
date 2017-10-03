8537e6f3edd2fbe5eb9744d31b1d45d639c066da
dreiv <dreivoicu@gmail.com>
2017-10-03 22:38:55 +0300

    adding animations.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,30 @@
+import { animate, state, style, transition, trigger } from '@angular/animations';
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  template: `
-    <p>
-      testing CI/CD!
-    </p>
-  `,
-  styles: []
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.scss'],
+	// animations metadata, we define a trigger with two possible states
+	animations: [
+		trigger('movable', [
+			state('fixed', style({})),
+			state('roaming', style({
+				'background-color': 'green',
+				'left': '90%'
+			})),
+			// define a transition from one state to another, and an associated animation
+			transition('* => *', animate('5s 0s ease-in'))
+		])
+	]
 })
 export class AppComponent {
-  title = 'app';
+	title = 'Stock Dashboard';
+	moving: string;
+
+	moveIt() {
+		console.log('on the move');
+		// change the state of moving to roaming, activating the trigger and animation
+		this.moving = 'roaming';
+	}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,18 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent
+	],
+	imports: [
+		BrowserAnimationsModule,
+		BrowserModule
+	],
+	providers: [],
+	bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
