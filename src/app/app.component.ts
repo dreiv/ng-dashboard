2e0089ab7a369ec027a16b6a2803cefef463ebaa
dreiv <dreivoicu@gmail.com>
2017-10-04 21:16:40 +0300

    added stock service.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { animate, state, style, transition, trigger } from '@angular/animations';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { StockService } from './stock.service';
 
 @Component({
 	selector: 'app-root',
 	templateUrl: './app.component.html',
 	styleUrls: ['./app.component.scss'],
+	providers: [ StockService ],
 	// animations metadata, we define a trigger with two possible states
 	animations: [
 		trigger('movable', [
@@ -18,13 +20,32 @@ import { Component } from '@angular/core';
 		])
 	]
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
 	title = 'Stock Dashboard';
 	moving: string;
+	stockPrices = [];
+
+	constructor(private stockService: StockService) {
+
+	}
+
+	// once the component is created, poll the stock service regularly for fresh prices
+	ngOnInit(): void {
+		// check for updated prices
+		setInterval(() => {this.getStockPrices(); }, 1000);
+	}
 
 	moveIt() {
 		console.log('on the move');
 		// change the state of moving to roaming, activating the trigger and animation
 		this.moving = 'roaming';
+	}
+
+	getStockPrices() {
+		// call out asynchronously to the stock service for prices, then update prices once data is received back
+		this.stockService.getStockPrices().then(prices => {
+			console.log('just got prices: ', prices);
+			this.stockPrices = prices;
+		});
 	}
 }
